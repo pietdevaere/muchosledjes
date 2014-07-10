e51ef58d33ae41c62a323777cabf7fe5694bbd0e
pietdevaere <piet@devae.re>
2014-07-10 09:47:20 +0200

    working version to show static text

--- fonttest.py
+++ fonttest.py
@@ -2,11 +2,10 @@ import socket
 import time
 import math
 
-font = {'A':['01110', '10001', '10001', '10001', '11111', '10001', '10001'],
-        'B':['11110', '10001', '10001', '11110', '10001', '10001', '11110'],
-        'C':['01110', '10001', '10000', '10000', '10000', '10001', '01110']}
 
 def text_to_bin(text):
+    """Converts text to 7 rows of binary data, 1 represents a lit pixel"""
+
     result = ['' for i in range(7)]
     text = text[0:36]  ## truncate the text to the maximum lenght
     for letter in text:
@@ -17,10 +16,10 @@ def text_to_bin(text):
     return result
 
 def group_to_decimal(group):
+    """Converts a group of 8 binary digits to it's decimal value, outdated"""
     result = 0
     value = 128
     for bit in group:
-##        print("  bit = {}, int(bit)= {}, value = {}".format(bit, int(bit), value))
         result += int(bit) * value
         value /= 2
     return result
@@ -31,7 +30,8 @@ def bin_to_decarray(binary):
         groups = [row[i:i+8] for i in range(0, len(row), 8)]
         decrow = []
         for group in groups:
-            decrow.append(group_to_decimal(group))
+            decrow.append(int(group, 2))
+         ##   decrow.append(group_to_decimal(group))
         decrow = decrow + (27 - len(decrow)) * [0] ## pad with zeros
         result.append(decrow)
     return(result)
@@ -40,11 +40,32 @@ def decarray_to_bytestream(decarray):
     bytestream = []
     for row in decarray:
         bytestream += row
-    print bytestream
+##    print bytestream
     return bytearray(bytestream)
-        
-            
-            
+
+def static_text(message, quite = 0):
+    if len(message) > 36 and quite == 0:
+        print("Message to long, will be shorted")
+    frame = decarray_to_bytestream(bin_to_decarray(text_to_bin(message)))
+
+    sock = socket.socket(socket.AF_INET, # Internet
+        socket.SOCK_DGRAM) # UDP
+    sock.sendto(frame, (UDP_IP, UDP_PORT))
+
+def scrolling_text(message):
+    pass    
+
+
+font = {' ':['00000', '00000', '00000', '00000', '00000', '00000', '00000']}
+
+fontFile = open('ledFont', 'r')
+
+for line in fontFile:
+    line = line.strip()
+    line = line.split(' ')
+    font[line[0]] = line[1:][::-1]
+
+fontFile.close()
 
 UDP_IP = "10.23.5.143"
 UDP_PORT = 5000
@@ -54,10 +75,4 @@ MESSAGE = "Hello, World!"
 ##print "UDP target port:", UDP_PORT
 ##print "message:", MESSAGE
 
-MESSAGE = decarray_to_bytestream(bin_to_decarray(text_to_bin('ABC'*50)))
-
-sock = socket.socket(socket.AF_INET, # Internet
-                             socket.SOCK_DGRAM) # UDP
-
-
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+static_text('The quick brown fox jumps over the lazy dog')
