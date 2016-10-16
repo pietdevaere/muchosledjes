503e2032c439ce91050973eb9c5abd60803f4eaf
Piet De Vaere <piet@devae.re>
2016-10-16 17:50:45 +0200

    script to reload program from external sources

--- getConfig.py
+++ getConfig.py
@@ -4,27 +4,148 @@ import requests
 import datetime
 import dateutil.parser
 import os.path
+import os
+import urllib.request
+import subprocess
+import sys
+import time
+import shutil
 
-remote_url = "http://kiekehoning.be/bla.txt"
-local_url = "/tmp/bla.txt"
+SERVER_URL = "http://kiekehoning.be/bla.py"
+LOCAL_URL = "/tmp/bla.py"
+POLLING_TIME = 60
 
-def get_local_mtime():
-    
-    #if the file does not exist
-    if not os.path.isfile(local_url):
+class File():
+    def __init__(self,  url):
+        self.url = url
+
+    def get_url(self):
+        return self.url
+
+class LocalFile(File):
+    def mtime(self):
+        #if the file does not exist
+        if not self.exists():
+            return None
+        
+        timestamp = os.path.getmtime(self.url)
+        mtime = datetime.datetime.fromtimestamp(timestamp, datetime.timezone.utc)
+        return mtime
+
+    def get(self, destination):
+       shutil.copyfile(self.url, destination)
+
+    def exists(self):
+        return os.path.isfile(self.url)
+        
+
+class HttpFile(File):
+    def mtime(self):
+        try:
+            res = requests.request('HEAD', self.url)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            return None
+
+        if res.status_code == 200:
+            try:
+                mtime = dateutil.parser.parse(res.headers['last-modified'])
+            except KeyError:
+                return None
+            return mtime
+
         return None
+        
+    def get(self, destination):
+        try:
+            print('--> Trying to get the file over HTTP')
+            urllib.request.urlretrieve(self.url, destination)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except Exception as e:
+            print(e)
+            return False
+        else:
+            return True
+                                        
+class MonitoredFile():
+    def __init__(self, source_file):
+        self.file = source_file
+        self.sources = []
 
-    timestamp = os.path.getmtime(local_url)
-    mtime = datetime.datetime.fromtimestamp(timestamp, datetime.timezone.utc)
+    def add_source(self, source_file):
+        self.sources.append(source_file)
+   
+    def get_latest_source(self):
+        latest_mtime = self.file.mtime()
+        latest_source = None
+
+        for source in self.sources:
+            source_mtime = source.mtime()
+            if source_mtime == None:
+                continue
+            if source_mtime > latest_mtime:
+                latest_mtime = source_mtime
+                latest_source = source
+
+        if latest_source != None:
+            print('--> Found a newer version of the file!')
+            print('--> at path: ' + latest_source.get_url())
+
+        return latest_source
+
+    def update(self):
+        source = self.get_latest_source()
+        if source:
+            source.get(self.get_url())
+            os.chmod(self.get_url(), 0o744)
+                
+    def exists(self):
+        return True 
+
+    def get_url(self):
+        return self.file.get_url()
+
+class Process():
+    def __init__(self, executable):
+        self.executable = executable
+        self.start_time = None
+        self.process = None
+
+    def run_latest(self):
+        if not self.executable.exists():
+            return
+
+        if self.start_time == None or self.executable.mtime() > self.start_time:
+            print('--> File changed, restarting')
+            self.kill()  # beter be safe!
+            self.start_time = datetime.datetime.now(tz=datetime.timezone.utc)
+            self.start()            
+
+    def start(self):
+        self.process = subprocess.Popen(self.executable.get_url())
+
+    def kill(self):
+        if self.process:
+            self.process.kill()
+
+
+if __name__ == "__main__":
+
+    process_file = MonitoredFile(LocalFile(LOCAL_URL))
+    process_file.add_source(HttpFile(SERVER_URL))
+    process_file.add_source(LocalFile('/tmp/blo.py')) 
+    process = Process(LocalFile(LOCAL_URL))
+
+    try:
+        while True:
+            process_file.update()
+            process.run_latest()
+            time.sleep(1)
+    except:
+        raise
+    finally:
+        process.kill()
     
-    return mtime
-
-def get_remote_mtime():
-    res = requests.request('HEAD', remote_url)
-    mtime = dateutil.parser.parse(res.headers['last-modified'])
-    
-    return mtime
-
-def check_if_remote_is_new():
-    return get_remote_mtime() > get_local_mtime()
-
+    sys.exit()
